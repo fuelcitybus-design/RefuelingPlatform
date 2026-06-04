5700955a60d2fc690ebf777e08ab4b02dd0cac5c
fuelcitybus-design <fuelcitybus@gmail.com>
2026-06-04 16:34:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,17 +72,21 @@ ROOT_FOLDER = f"https://{KUDU_HOST}/api/vfs/data"
 ###Module 1/O: Uploader camera forced setting
 def prefer_back_camera():
     custom_html = """
+    <style>
+    /* Flip the preview back to normal */
+    video, img {
+      transform: scaleX(-1);
+    }
+    </style>
     <script>
     const originalGetUserMedia = navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices);
 
     navigator.mediaDevices.getUserMedia = (constraints) => {
       if (!constraints.video.facingMode) {
-        constraints.video.facingMode = {ideal: "environment"};
+        constraints.video.facingMode = {ideal: "environment"}; // prefer back camera
       }
-
       constraints.video.width = {exact: 400};
       constraints.video.height = {exact: 400};
-
       return originalGetUserMedia(constraints);
     };
     </script>
