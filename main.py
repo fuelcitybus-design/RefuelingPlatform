73aecc35548d4837df6875442e8aa94e8213bd7b
fuelcitybus-design <fuelcitybus@gmail.com>
2026-06-11 11:01:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -733,6 +733,10 @@ with gr.Blocks(head=prefer_back_camera()) as demo: # DeprecationWarning: The 'he
             #camera_input label {
                 font-size: 20px !important;
             }
+
+            #camera_input .device-selector {
+                display: none !important;
+            }
                         
             """
         
