eb8686db37157708c7f1cfae18e686423d655cd6
fuelcitybus-design <fuelcitybus@gmail.com>
2026-06-15 10:53:36 +0800

    Update mystart.sh

--- mystart.sh
+++ mystart.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Runtime startup script for Azure App Service
 
-# Ensure headless OpenCV is installed (safe in sandbox)
-pip uninstall -y opencv-python
-pip install opencv-python-headless
 python -m gunicorn main:app -k uvicorn.workers.UvicornWorker
