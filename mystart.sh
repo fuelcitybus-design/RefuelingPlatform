47311c0911924016fa0b06483747cba3e3b312e8
fuelcitybus-design <fuelcitybus@gmail.com>
2026-06-15 11:58:22 +0800

    Update mystart.sh

--- mystart.sh
+++ mystart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
 
-python -m gunicorn main:app -k uvicorn.workers.UvicornWorker
+exec gunicorn main:app \
+    --workers 4 \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --bind 0.0.0.0:${PORT:-8000}
