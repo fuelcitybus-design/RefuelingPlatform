2fac4b9dd4bc907147be5465652e7065b93c70f2
fuelcitybus-design <fuelcitybus@gmail.com>
2026-06-30 00:40:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -417,7 +417,6 @@ with gr.Blocks(head=prefer_back_camera()) as demo: # DeprecationWarning: The 'he
             #car_dropdown.change(clear_images, location_dropdown, image_inputs)
             #tank_dropdown.change(clear_images, location_dropdown, image_inputs)
 
-            #button.icon for camera button camera selection dropdown(.select-wrap) with its button + upload option toggle
             demo.css = """
             #camera_input button {
                 transform: scale(1.6);   /* Scaling for making all gr.image buttons larger */
@@ -441,11 +440,16 @@ with gr.Blocks(head=prefer_back_camera()) as demo: # DeprecationWarning: The 'he
                 font-size: 20px !important;
             }
 
+            
+            #camera_input .dropdown-arrow {
+                display: none !important;
+            }
+
             #camera_input .select-wrap {
                 display: none !important;
             }
 
-            #camera_input button.icon {
+            #camera_input button-wrap.button.icon {
                 display: none !important;
             }
                         
