dfd5a782ff9b738426c277126f89eb7e55c89e84
fuelcitybus-design <fuelcitybus@gmail.com>
2026-07-20 14:04:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -791,7 +791,5 @@ with gr.Blocks(head=prefer_back_camera()) as demo: # DeprecationWarning: The 'he
             }
                         
             """
-        
-        
-        
+
 app = gr.mount_gradio_app(app, demo, path="/")
