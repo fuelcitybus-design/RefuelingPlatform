dca3dbdf7f34d4d914cb1ddeb971294a78f2d157
fuelcitybus-design <fuelcitybus@gmail.com>
2026-08-11 22:22:50 +0800

    Add global result_text variable for message storage
    Added global variable result_text to store messages.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Setup environment for running Gradio interface
+on't#Setup environment for running Gradio interface
 from fastapi import FastAPI
 app = FastAPI()
 
@@ -227,6 +227,8 @@ global active_tabs
 active_tabs = []
 global tank_choices
 tank_choices = []
+global result_text
+result_text = ""
 
 # Single synchronous save handler (no global messages)
 def save_images(location, car_id, tank_id, *images, request=None):
@@ -361,7 +363,7 @@ def save_images(location, car_id, tank_id, *images, request=None):
         else:
             if not messages_local:
                 messages_local.append(str("警告：沒有新照片"))
-
+        result_text = ""
         result_text = "\n".join([str(m) for m in messages_local])
         result_text = result_text.encode("utf-8", "ignore").decode("utf-8")
         print(f"[{datetime.now().isoformat()}] RETURNING: {repr(result_text)}", file=sys.stderr, flush=True)
