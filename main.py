b994883b643489addc08b0bf260b371d14c50f20
fuelcitybus-design <fuelcitybus@gmail.com>
2026-09-16 16:18:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1257,6 +1257,7 @@ with gr.Blocks(head=prefer_back_camera(), css="#status-bar { font-weight: bold; 
                 fn=save_images,
                 inputs=[location_dropdown, car_dropdown, tank_dropdown] + image_inputs,
                 outputs=output_text
+                concurrency_limit=1
             )
             
             confirm_btn.click(
